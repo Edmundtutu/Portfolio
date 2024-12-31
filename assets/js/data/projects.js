1d1f9443063d2b6a2785238d3eec1f15dea853b4
Edmundtutu <2021bse180@std.must.ac.ug>
2024-12-31 12:48:44 +0300

    Projects

--- assets/js/data/projects.js
+++ assets/js/data/projects.js
@@ -1,31 +1,72 @@
 const projects = [
     {
-        title: "Edmund's Project 1",
+        title: "Ovum",
         description: "A project by Edmund Tutuma showcasing...",
-        image: "/assets/images/ai.jpg",
-        technologies: ["HTML", "CSS", "JavaScript"],
+        image: "/assets/images/ovum.webp",
+        technologies: ["Android", "Java", "SQLite"],
         link: "#"
     },
     // Add more of your actual projects here
     {
-        title: "Edmund's Project 1",
+        title: "Ovum Doctor",
         description: "A project by Edmund Tutuma showcasing...",
-        image: "/assets/images/ai.jpg",
-        technologies: ["HTML", "CSS", "JavaScript"],
+        image: "/assets/images/ovumdoc.png",
+        technologies: ["HTML", "CSS", "JavaScript", "PHP","Vue"],
         link: "#"
     },
     {
-        title: "Edmund's Project 1",
+        title: "USSD Ovum interface",
         description: "A project by Edmund Tutuma showcasing...",
-        image: "/assets/images/ai.jpg",
-        technologies: ["HTML", "CSS", "JavaScript"],
+        image: "/assets/images/ussdovum.webp",
+        technologies: ["Lavarel", "MYSQL"],
         link: "#"
     },
     {
-        title: "Edmund's Project 1",
+        title: "Effortless JSON",
+        description: "A project by Edmund Tutuma showcasing...",
+        image: "/assets/images/json.webp",
+        technologies: ["Java", "XML"],
+        link: "#"
+    },
+    {
+        title: "Solving Computing problems with Algorithms",
+        description: "A project by Edmund Tutuma showcasing...",
+        image: "/assets/images/aps.webp",
+        technologies: ["Java", "Python"],
+        link: "#"
+    },
+    {
+        title: "Tommy the Math Teacher",
         description: "A project by Edmund Tutuma showcasing...",
         image: "/assets/images/ai.jpg",
-        technologies: ["HTML", "CSS", "JavaScript"],
+        technologies: ["LLMs", "GPT 4o mini", "OpenWebUi"],
+        link: "#"
+    }, {
+        title: "Flicky",
+        description: "A project by Edmund Tutuma showcasing...",
+        image: "/assets/images/flicky.webp",
+        technologies: ["LLMs", "llama 3", "OpenWebUi"],
+        link: "#"
+    },
+    {
+        title: "Sacco API",
+        description: "A project by Edmund Tutuma showcasing...",
+        image: "/assets/images/saccoapi.jpg",
+        technologies: ["Laravel"],
+        link: "#"
+    },
+    {
+        title: "Food Ordering System",
+        description: "A project by Edmund Tutuma showcasing...",
+        image: "/assets/images/foodsys.jpg",
+        technologies: ["FireBase", "Javascript"],
+        link: "#"
+    },
+    {
+        title: "Hotel Booking System",
+        description: "A project by Edmund Tutuma showcasing...",
+        image: "/assets/images/hotel.webp",
+        technologies: ["Laravel"],
         link: "#"
     },
 ];
